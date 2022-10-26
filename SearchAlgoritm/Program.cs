4eba358e559cb5e02d7734c2c46f346b180a77dc
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2022-10-26 17:40:42 +0700

    Add project files.

---
+++ SearchAlgoritm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgoritm
+{
+    class Program
+    {
+        //array to be searched
+        int[] arr = new int[20];
+        //number of elemenets in the array
+        int n;
+        //get the number of elements to store in the array
+        int i;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
